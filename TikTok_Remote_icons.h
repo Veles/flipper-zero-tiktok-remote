995e7875a91766a470cc5da901b863006c9da0aa
Veles <derunix@ya.ru>
2022-10-28 10:01:23 +0300

    first commit

---
+++ TikTok_Remote_icons.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <gui/icon.h>
+
+extern const Icon I_Arr_dwn_7x9;
+extern const Icon I_Arr_up_7x9;
+extern const Icon I_Ble_connected_15x15;
+extern const Icon I_Ble_disconnected_15x15;
+extern const Icon I_Button_18x18;
+extern const Icon I_Circles_47x47;
+extern const Icon I_Left_mouse_icon_9x9;
+extern const Icon I_Like_def_11x9;
+extern const Icon I_Like_pressed_17x17;
+extern const Icon I_Ok_btn_pressed_13x13;
+extern const Icon I_Pressed_Button_13x13;
+extern const Icon I_Right_mouse_icon_9x9;
+extern const Icon I_Space_65x18;
+extern const Icon I_Voldwn_6x6;
+extern const Icon I_Volup_8x6;
